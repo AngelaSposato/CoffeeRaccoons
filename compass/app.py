43398eb2a78918c862e058f1cace841d920e17b4
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2025-02-01 12:40:30 -0500

    Add temp app icon and center the map on montreal

--- compass/app.py
+++ compass/app.py
@@ -1,8 +1,8 @@
 from dash import Dash
 import dash_leaflet as dl
-
-app = Dash()
-app.layout = dl.Map(dl.TileLayer(), center=[56,10], zoom=6, style={'height': '50vh'})
+# 45.5019° N, 73.5674° W
+app = Dash(__name__, title='Montreal Compass')
+app.layout = dl.Map(dl.TileLayer(), center=[45.5019, -73.5674], zoom=6, style={'height': '50vh'})
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
